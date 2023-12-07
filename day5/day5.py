99e850d9540f14c97a0a1ec05fbe40ed13787a13
Jakub <kubaok96@gmail.com>
2023-12-07 22:44:32 +0100

    add solution for day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,150 +1,111 @@
 import pandas as pd
-from collections import deque
 
 
-def map_converter(seeds, toSoil, toFert, toWater, toLight, toTemp, toHumid, toLoc):
-    data = {}
-    # destination, source, range
-    # soil       , seed
-    for seed in seeds:
-        data[seed] = {}
-        # seed to soil
-        for soil in toSoil:
-            if soil[0] <= seed < soil[0] + soil[2]:
-                data[seed]["soil"] = seed - soil[0] + soil[1]
-        if not "soil" in data[seed]:
-            data[seed]["soil"] = seed
-
-    # soil to fertilizer
-
-    # fertilizer to water
-
-    # water to light
-
-    # light to temperature
-
-    # temperature to humidity
-
-    # humidity to location
-
-    return data
-
-
-def map_converter(seeds, map_dict):
+def map_converter_dict(seeds, map_dict):
+    # part 1
     data = {}
     # destination, source, range
     # soil       , seed
     keys = ["seed"] + list(map_dict.keys())
-    # print(keys)
     for seed in seeds:
         data[seed] = {"seed": seed}
         for i, name in enumerate(map_dict):
-            # print(f"{i=}")
             for prop in map_dict[name]:
-                # print(f"{prop=}")
                 if prop[1] <= data[seed][keys[i]] < (prop[1] + prop[2]):
                     data[seed][name] = data[seed][keys[i]] - prop[1] + prop[0]
-                    # print(
-                    #     f"{data[seed][keys[i]]=}, {data[seed][name]=}, {prop[0]=}, {prop[1]=}, {prop[2]=}"
-                    # )
-                    # print(f"{keys[i+1]=}")
                     break
             if not name in data[seed]:
                 data[seed][name] = data[seed][keys[i]]
-                # print(f"{keys[i+1]=}")
     return data
 
 
-def map_converter2(seeds, map_dict):
+def map_converter(seeds, map_dict):
+    # part 1
     # destination, source, range
     # soil       , seed
     min_location = 1756229080
-    for i in range(0, len(seeds) // 2 + 1, 2):
-        for j in range(seeds[i + 1]):
-            last_value = seeds[i] + j
-            for name in map_dict:
-                for prop in map_dict[name]:
-                    if prop[1] <= last_value < (prop[1] + prop[2]):
-                        last_value = last_value - prop[1] + prop[0]
-                        break
-                    # print(f"{keys[i+1]=}")
-            if last_value < min_location:
-                min_location = last_value
-    return min_location
-
-
-def map_converter3(seeds, map_dict):
-    # destination, source, range
-    # soil       , seed
-    min_location = 1756229080
-    # sort values bo source in mapdict
-    # sort seeds in pairs
-    # take first value from mapdict, if in range and checked at least once pop first element and take next
-    sorted_seeds = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds) // 2 + 1, 2)]
-    sorted_seeds.sort(key=lambda x: x[0])
-    [map_dict[name].sort(key=lambda x: x[1]) for name in map_dict]
-
-    seeds.sort()
-    for seed in [79]:
-        print(f"Checking seed {seed}")
-        prev_value = seed
-
-        # for seed in sorted_seeds:
-        #     print(f"Checking seed {seed[0]}")
-        #     for seed_range in range(seed[1]):
-        #         prev_value = seed[0]  + seed_range
+    for seed in seeds:
+        last_value = seed
         for name in map_dict:
-            if map_dict[name]:
-                source_range_end = map_dict[name][0][1] + map_dict[name][0][2]
-                if prev_value >= source_range_end:
-                    print(f"{name=}, {source_range_end=}")
-                    map_dict[name].pop(0)
-                    # break
-                elif map_dict[name][0][1] <= prev_value:
-                    prev_value = (
-                        prev_value - map_dict[name][0][1] + map_dict[name][0][0]
-                    )
-        if prev_value < min_location:
-            print(f"{seed=}")
-            min_location = prev_value
+            for prop in map_dict[name]:
+                if prop[1] <= last_value < (prop[1] + prop[2]):
+                    last_value = last_value - prop[1] + prop[0]
+                    break
+        if last_value < min_location:
+            min_location = last_value
     return min_location
 
 
-def map_converter3(seeds, map_dict):
+def get_ranges(init_ranges, mappings):
+    final_ranges = []
+    if isinstance(init_ranges, list):
+        ranges_to_check = init_ranges
+    else:
+        ranges_to_check = [init_ranges]
+    while ranges_to_check:
+        range1 = ranges_to_check[0]
+        prev_checked = ranges_to_check.copy()
+        for dest_start, source_start, range_len in mappings:
+            range2 = (source_start, source_start + range_len)
+            dest = (dest_start, dest_start + range_len)
+            if range1[0] >= range2[0]:  # 2,3,5
+                if range1[1] <= range2[1]:  # 2
+                    mapped_start = dest[0] + range1[0] - source_start
+                    mapped = (mapped_start, mapped_start + range1[1] - range1[0])
+                    final_ranges.append(mapped)
+                    ranges_to_check.pop(0)
+                    break
+                if range1[0] >= range2[1]:  # 3
+                    continue
+                if range1[1] > range2[1]:  # 5
+                    if range1[0] < range2[1]:
+                        mapped_start = dest[0] + range1[0] - range2[0]
+                        mapped = (mapped_start, dest[1])
+                        final_ranges.append(mapped)
+                    ranges_to_check.append((range2[1], range1[1]))
+                    ranges_to_check.pop(0)
+                    break
+            if range1[1] <= range2[0]:  # 1
+                continue
+            if range1[1] <= range2[1]:  # 4
+                mapped = (dest[0], dest[0] + range1[1] - range2[1])
+                final_ranges.append(mapped)
+                ranges_to_check.append((range1[0], range2[0]))
+                ranges_to_check.pop(0)
+                break
+            else:  # 6
+                ranges_to_check.append((range1[0], range2[0]))
+                ranges_to_check.append((range2[1], range1[1]))
+                final_ranges.append(dest)
+                ranges_to_check.pop(0)
+                break
+        if ranges_to_check and ranges_to_check[0] == range1:
+            ranges_to_check.pop(0)
+            final_ranges.append(range1)
+        if prev_checked == ranges_to_check:
+            break
+
+    final_ranges.extend(ranges_to_check)
+    return final_ranges
+
+
+def map_converter_by_ranges(seeds, map_dict):
+    # part 2
     # destination, source, range
     # soil       , seed
-    min_location = 1756229080
-    # sort values bo source in mapdict
-    # sort seeds in pairs
-    # take first value from mapdict, if in range and checked at least once pop first element and take next
-    sorted_seeds = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
 
+    sorted_seeds = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
     sorted_seeds.sort(key=lambda x: x[0])
     [map_dict[name].sort(key=lambda x: x[1]) for name in map_dict]
-    print(f"{sorted_seeds=}")
+    ranges = [
+        (range_start, range_start + range_len)
+        for range_start, range_len in sorted_seeds
+    ]
 
-    for seed in sorted_seeds:
-        print(f"Checking seed {seed[0]}")
-        for seed_range in range(seed[1]):
-            prev_value = seed[0] + seed_range
-            for name in map_dict:
-                while map_dict[name]:
-                    source_range_end = map_dict[name][0][1] + map_dict[name][0][2]
-                    if prev_value > source_range_end:
-                        # print(f"{name=}, {source_range_end=}")
-                        map_dict[name].pop(0)
-                    elif map_dict[name][0][1] <= prev_value:
-                        prev_value = (
-                            prev_value - map_dict[name][0][1] + map_dict[name][0][0]
-                        )
-                        break
-                    else:
-                        break
-            if prev_value < min_location:
-                # print(f"{seed=}")
-                min_location = prev_value
-        print(f"Current minimum: {min_location}")
-    return min_location
+    for mappings in map_dict.values():
+        ranges = get_ranges(ranges, mappings)
+    min_location = min(ranges)
+    return min_location[0]
 
 
 def one_star(filename):
@@ -158,10 +119,15 @@ def one_star(filename):
         )
         for line in coded_input[1:]
     }
+    # dict solution
+    # data = map_converter_dict(seeds, map_dict)
+    # df = pd.DataFrame([el[1] for el in list(data.items())])
+    # print(df)
+    # print(f'Part 1 minimum location is: {df.min()["humidity-to-location map"]}')
 
     data = map_converter(seeds, map_dict)
-    df = pd.DataFrame([el[1] for el in list(data.items())])
-    print(df.min()["humidity-to-location map"])
+    print(data)
+    print(f"Part 1 minimum location is: {data}")
 
 
 def two_stars(filename):
@@ -175,10 +141,9 @@ def two_stars(filename):
         )
         for line in coded_input[1:]
     }
-
-    min_location = map_converter3(seeds, map_dict)
-    print(f"Minimum location is: {min_location}")
+    min_location = map_converter_by_ranges(seeds, map_dict)
+    print(f"Part 2 minimum location is: {min_location}")
 
 
 # one_star("day5_input.txt")
-two_stars("day5_input2.txt")
+# two_stars("day5_input.txt")
